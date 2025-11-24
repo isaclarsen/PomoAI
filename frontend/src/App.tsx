acf77f2ed052e32ef3efb787579d4be71de06fe3
isaclarsen <isaclarsen22@gmail.com>
2025-11-24 22:54:47 +0100

    Added relax view

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,11 @@ import GuestStartView from './components/GuestStartView';
 import FocusTimerView from './components/FocusTimerView';
 import QuestionResultView from './components/QuestionResultView';
 import { startGuestSession, updateGuestSessionStatus, type QuestionDTO } from './api/pomoApi';
+import RelaxTimerView from './components/RelaxTimerView';
 
 function App({}) {
 
-  const [currentView, setCurrentView] = useState<"guestStart" | "focusTimer" | "questionResult">("guestStart");
+  const [currentView, setCurrentView] = useState<"guestStart" | "focusTimer" | "relaxTimer" | "questionResult">("guestStart");
   const [topic, setTopic] = useState("");
   const [questions, setQuestions] = useState<QuestionDTO[]>([]);
   const [sessionId, setSessionId] = useState<number>(0);
@@ -26,17 +27,22 @@ function App({}) {
 
   const handleTimerFinished = async (sessionId : number) => {
     try {
+      setCurrentView("relaxTimer")
       setQuestions(await updateGuestSessionStatus("COMPLETED", sessionId));
-      setCurrentView("questionResult")
     } catch (error) {
       console.error("Failed to update status on Pomo Session", error)
     }
+  }
+
+  const handleRelaxTimerFinished = async () => {
+    setCurrentView("questionResult")
   }
   
   return (
       <>
       {currentView === "guestStart" && <GuestStartView onStart={handleStartSession}/>}
       {currentView === "focusTimer" && <FocusTimerView onTimerFinished={() => handleTimerFinished(sessionId)}/>}
+      {currentView === "relaxTimer" && <RelaxTimerView onTimerFinished={() => handleRelaxTimerFinished()}/>}
       {currentView === "questionResult" && <QuestionResultView questions={questions} onReset={() => setCurrentView("guestStart")}/>}
       </>
 
---
+++ frontend/src/components/RelaxTimerView.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+interface FocusTimerViewProps {
+    onTimerFinished: () => void;
+}
+
+function RelaxTimerView({onTimerFinished} : FocusTimerViewProps){
+
+        const [timeLeft, setTimeLeft] = useState(15)
+
+        useEffect(() => {
+            if (!timeLeft) return;
+
+            const intervalId = setInterval(() => {
+                setTimeLeft((prevTime) => {
+                    if(prevTime <= 0){
+                        return 0
+                    }
+                    return prevTime - 1
+                })
+            }, 1000)
+
+            return () => clearInterval(intervalId)
+        }, []);
+
+        useEffect(() => {
+            if(timeLeft === 0){
+                onTimerFinished();
+            }
+
+        }, [timeLeft, onTimerFinished])
+
+        const formatTime = (seconds: number) => {
+            const m = Math.floor(seconds / 60);
+            const s = seconds % 60;
+            return `${m}:${s < 10 ? '0' : ''}${s}`;
+        }
+
+    return (
+        <div>
+            <h2>Bra jobbat! Ta en liten paus!</h2>
+            <h3>{formatTime(timeLeft)}</h3>
+            <button onClick={onTimerFinished}>
+                Skippa timer (dev)
+            </button>
+        </div>
+    )
+}
+
+export default RelaxTimerView;
